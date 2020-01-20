17e85930a97e6d9091161d790a089293892804fe
Jugal <jugaldeepak@gmail.com>
2020-01-21 02:44:17 +0530

    sample scaffolds

--- lib/views/basic_views/scaffold_views/scaffold_view.dart
+++ lib/views/basic_views/scaffold_views/scaffold_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:widget_learner/views/basic_views/scaffold_views/scaffold_with_app_bar_view.dart';
+import 'package:widget_learner/views/basic_views/scaffold_views/scaffold_with_body_view.dart';
+import 'package:widget_learner/views/basic_views/scaffold_views/scaffold_with_button_view.dart';
+import 'package:widget_learner/views/basic_views/scaffold_views/simple_scaffold_view.dart';
+import 'package:widget_learner/widgets/custom_card.dart';
 import 'package:widget_learner/widgets/scaffolds/custom_scaffold.dart';
 
 class ScaffoldView extends StatelessWidget {
@@ -6,8 +11,59 @@ class ScaffoldView extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScaffold(
       title: 'Scaffold',
-      body: Center(
-        child: Text('Scaffold View'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 8, 20, 4),
+            child: Text(
+              'Sample Scaffolds',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Color.fromRGBO(0, 0, 0, 0.8),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                CustomCard(
+                  title: 'Simple Scaffold',
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => SimpleScaffoldView(),
+                    ),
+                  ),
+                ),
+                CustomCard(
+                  title: 'Scaffold with AppBar',
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ScaffoldWithAppBarView(),
+                    ),
+                  ),
+                ),
+                CustomCard(
+                  title: 'Scaffold with Body',
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ScaffoldWithBodyView(),
+                    ),
+                  ),
+                ),
+                CustomCard(
+                  title: 'Scaffold with Floating Action Button',
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ScaffoldWithButtonView(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
